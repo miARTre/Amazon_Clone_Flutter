f4df6a5280ced63b1346f38a2028f4f5415c572f
miARTre <mirnesbb@gmail.com>
2022-11-24 19:02:41 +0100

    Home screen UI

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 String uri = 'http://localhost:3000';
+
 class GlobalVariables {
   static const appBarGradient = LinearGradient(
     colors: [
@@ -23,5 +24,28 @@ class GlobalVariables {
     'https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wireless/AdvantagePrime/BAU/14thJan/D37196025_IN_WL_AdvantageJustforPrime_Jan_Mob_ingress-banner_1242x450.jpg',
     'https://images-na.ssl-images-amazon.com/images/G/31/Symbol/2020/00NEW/1242_450Banners/PL31_copy._CB432483346_.jpg',
     'https://images-na.ssl-images-amazon.com/images/G/31/img21/shoes/September/SSW/pc-header._CB641971330_.jpg',
+  ];
+
+  static const List<Map<String, String>> categoryImages = [
+    {
+      'title': 'Mobiles',
+      'image': 'assets/images/mobiles.jpeg',
+    },
+    {
+      'title': 'Essentials',
+      'image': 'assets/images/essentials.jpeg',
+    },
+    {
+      'title': 'Appliances',
+      'image': 'assets/images/appliances.jpeg',
+    },
+    {
+      'title': 'Books',
+      'image': 'assets/images/books.jpeg',
+    },
+    {
+      'title': 'Fashion',
+      'image': 'assets/images/fashion.jpeg',
+    },
   ];
 }
--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,8 +1,10 @@
-import 'package:flu/providers/user.provider.dart';
+import 'package:flu/features/home/widgets/carousel_image.dart';
+import 'package:flu/features/home/widgets/deal_of_day.dart';
+import 'package:flu/features/home/widgets/top_categories.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import '../../../constants/global_variables.dart';
+import '../widgets/address_box.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = '/home';
@@ -16,10 +18,9 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    final user = Provider.of<UserProvider>(context).user;
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(50),
+        preferredSize: const Size.fromHeight(60),
         child: AppBar(
           flexibleSpace: Container(
             decoration: const BoxDecoration(
@@ -69,18 +70,46 @@ class _HomeScreenState extends State<HomeScreen> {
                             width: 1,
                           ),
                         ),
+                        hintText: 'Search Amazon.de',
+                        hintStyle: const TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 17,
+                        ),
                       ),
                     ),
                   ),
+                ),
+              ),
+              Container(
+                color: Colors.transparent,
+                height: 42,
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                ),
+                child: const Icon(
+                  Icons.mic,
+                  color: Colors.black,
+                  size: 25,
                 ),
               ),
             ],
           ),
         ),
       ),
-      body: Center(
-        child: Text(
-          user.toJson(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: const [
+            AddressBox(),
+            SizedBox(
+              height: 10,
+            ),
+            TopCategories(),
+            SizedBox(
+              height: 10,
+            ),
+            CarouselImage(),
+            DealOfDay(),
+          ],
         ),
       ),
     );
